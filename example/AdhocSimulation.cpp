cb06dc6500b051a6f50006ee1dad2ce8a4d3f07c
J. K. Lee <44741178+jklee95@users.noreply.github.com>
2022-01-23 01:04:27 +0900

    docs: Update example/AdhocSimulation.cpp

--- example/AdhocSimulation.cpp
+++ example/AdhocSimulation.cpp
@@ -34,15 +34,6 @@ vector<Vertex>& AdhocSimulation::iGetVertice()
 		Vertex({ XMFLOAT3(-0.5f, +0.5f, 0.0f) }),
 		Vertex({ XMFLOAT3(+0.5f, +0.5f, 0.0f) }),
 		Vertex({ XMFLOAT3(+0.5f, -0.5f, 0.0f) })
-
-		//Vertex({ XMFLOAT3(-1.0f, -1.0f, -1.0f) }),
-		//Vertex({ XMFLOAT3(-1.0f, +1.0f, -1.0f) }),
-		//Vertex({ XMFLOAT3(+1.0f, +1.0f, -1.0f) }),
-		//Vertex({ XMFLOAT3(+1.0f, -1.0f, -1.0f) }),
-		//Vertex({ XMFLOAT3(-1.0f, -1.0f, +1.0f) }),
-		//Vertex({ XMFLOAT3(-1.0f, +1.0f, +1.0f) }),
-		//Vertex({ XMFLOAT3(+1.0f, +1.0f, +1.0f) }),
-		//Vertex({ XMFLOAT3(+1.0f, -1.0f, +1.0f) }) //, XMFLOAT4(Colors::Black)
 	};
 
 	return _vertices;
@@ -54,27 +45,7 @@ vector<unsigned int>& AdhocSimulation::iGetIndice()
 	{
 		// front face
 		0, 1, 2,
-		0, 2, 3,
-
-		//// back face
-		//4, 6, 5,
-		//4, 7, 6,
-
-		//// left face
-		//4, 5, 1,
-		//4, 1, 0,
-
-		//// right face
-		//3, 2, 6,
-		//3, 6, 7,
-
-		//// top face
-		//1, 5, 6,
-		//1, 6, 2,
-
-		//// bottom face
-		//4, 0, 3,
-		//4, 3, 7
+		0, 2, 3
 	};
 
 	return _indices;
